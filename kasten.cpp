e4d1ed6a59564a610332f9b555432e739f3aa47e
DJPercent <flo-warko@myway.de>
2017-11-09 14:26:56 +0100

    Initial commit

---
+++ kasten.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+
+#define CONSOLE_VISIBLE "N"
+#define CREATE_LOGFILE "Y"
+#define FILENAME_LOGFILE "logfile.txt"
+
+using namespace std;
+
+int init(void) {
+	if(CONSOLE_VISIBLE == "N") {
+		FreeConsole();
+	}
+	if(CREATE_LOGFILE == "Y") {
+		ofstream(FILENAME_LOGFILE);
+	}
+		
+}
+
+int main(int argc, char** argv) {
+	init();
+	
+	return 0;
+}
+
+
